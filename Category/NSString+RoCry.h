8bbbf3800f9322fd9c91a72e3c7ee431a337d58f
Xia Tian <tian.xia1@renren-inc.com>
2012-06-29 09:48:37 +0800

    gitignore and base64

---
+++ Category/NSString+RoCry.h
@@ -0,0 +1,16 @@
+//
+//  NSString+RoCry.h
+//  GithubMobile
+//
+//  Created by Tian Xia on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (RoCry)
+
+// from AFNetworking
+- (NSString *)base64EncodedStringFromString;
+
+@end
