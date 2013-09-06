07f9bb0f5e922f86bd241ace3b80c4c007912890
Summer <txia@avos.com>
2013-09-06 15:33:17 +0800

    add random color

---
+++ Classes/iOS/Category/UIColor+RoCry.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+RoCry.h
+//  oh-my-category
+//
+//  Created by Summer on 13-9-6.
+//  Copyright (c) 2013年 7782. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (RoCry)
+
++ (UIColor *)randomColor;
+
+@end
