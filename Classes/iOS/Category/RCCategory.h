af1d09341a6160ece5e980d011110f8ac7bf68a5
RoCry <crysheen@gmail.com>
2014-01-23 08:39:11 +0800

    refactor

---
+++ Classes/iOS/Category/RCCategory.h
@@ -0,0 +1,15 @@
+//
+//  RCCategory.h
+//  oh-my-category
+//
+//  Created by RoCry on 1/23/14.
+//  Copyright (c) 2014 FeN. All rights reserved.
+//
+
+@interface UIColor (RoCry)
+
++ (UIColor *)randomColor;
++ (UIColor*)colorWithHex:(NSInteger)hexValue alpha:(CGFloat)alphaValue;
++ (UIColor*)colorWithHex:(NSInteger)hexValue;
+
+@end
---
+++ Classes/iOS/Category/RCCategory.m
@@ -0,0 +1,32 @@
+//
+//  RCCategory.m
+//  oh-my-category
+//
+//  Created by RoCry on 1/23/14.
+//  Copyright (c) 2014 FeN. All rights reserved.
+//
+
+#import "RCCategory.h"
+
+@implementation UIColor (RoCry)
+
++ (UIColor *)randomColor {
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+}
+
++ (UIColor*)colorWithHex:(NSInteger)hexValue alpha:(CGFloat)alphaValue
+{
+    return [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0
+                           green:((float)((hexValue & 0xFF00) >> 8))/255.0
+                            blue:((float)(hexValue & 0xFF))/255.0 alpha:alphaValue];
+}
+
++ (UIColor*)colorWithHex:(NSInteger)hexValue
+{
+    return [UIColor colorWithHex:hexValue alpha:1.0];
+}
+
+@end
